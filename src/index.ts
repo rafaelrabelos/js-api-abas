25f6543f5377032bfa67fa2c34847055a1593f70
Rafael Rabelo <rafael_rabelo@live.com>
2020-10-16 22:04:08 -0300

    Registra aliases de import

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
 import 'module-alias/register';
+import path = require('path');
+import modules from 'module-alias';
+
+const upDir = (baseDir: string) => path.resolve( baseDir + '/..');
+
+modules.addAliases({
+    "@root": upDir(__dirname) ,
+    "@src": __dirname,
+    "@app": __dirname + "/application",
+    "@presentation": __dirname + "/presentation",
+    "@infra": __dirname + "/infra",
+    "@domain": __dirname + "/domain",
+    "@environment": upDir(__dirname) + "/environments/"
+});
 
 import { Application } from './application';
 
